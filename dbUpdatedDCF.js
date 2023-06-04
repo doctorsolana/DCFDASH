e7bc551a5e0e7937a5b8ac203cbb3193fa0a2b3e
Your Name <you@example.com>
2023-06-04 23:24:44 +0200

    Initial commit

---
+++ dbUpdatedDCF.js
@@ -0,0 +1,129 @@
+const { Connection, PublicKey } = require("@solana/web3.js");
+const sqlite3 = require("sqlite3").verbose();
+
+const DCF_FEE_WALLET = new PublicKey("h2oMkkgUF55mxMFeuUgVYwvEnpV5kRbvHVuDWMKDYFC");
+const DCF_HOUSE_WALLET = new PublicKey("EWBFhigrnx6q5MMaGgxcg22dZtqTcZwiut8ZG7QCAFo");
+const DCD_FEE_WALLET = new PublicKey("9dkpTtxnsDmJRh95QhkHsAUXasQwrmX6LE4AEVSVgn3Z");
+const DCD_HOUSE_WALLET = new PublicKey("7iCme5EXAmigk84QXJmsVztdrLNxaWfq5rz85vM5utPu");
+
+const rpc = "https://rpc.helius.xyz/?api-key=d1b8ca10-0ab9-4f59-9568-18a686943a7f";
+
+// Initialize connection 
+const connection = new Connection(rpc);
+
+
+let db;
+try {
+  db = new sqlite3.Database("balances.db");
+} catch (error) {
+  console.error("Failed to connect to the database:", error);
+  process.exit(1);
+}
+
+
+// Create the transactions table if it doesn't exist
+db.serialize(() => {
+  db.run(
+    `
+    CREATE TABLE IF NOT EXISTS balances (
+      blockTime INTEGER,
+      DCFFeeWalletBalance INTEGER,
+      DCFHouseWalletBalance INTEGER,
+      DCDFeeWalletBalance INTEGER,
+      DCDHouseWalletBalance INTEGER
+    )
+  `,
+    (err) => {
+      if (err) {
+        console.error("Error creating table:", err);
+        return;
+      }
+      console.log("Table created.");
+    }
+  );
+});
+
+const getBalanceWithRetry = async (wallet, retries = 5) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await connection.getBalance(wallet);
+    } catch (error) {
+      console.error(`getBalance failed for ${wallet}, retrying...`);
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // wait before retrying
+    }
+  }
+  throw new Error(
+    `Failed to get balance for ${wallet} after ${retries} attempts`
+  );
+};
+
+const getBalances = async () => {
+  console.log("Getting balances...");
+  const DCFFeeWalletBalance = await getBalanceWithRetry(DCF_FEE_WALLET) /1000000000;
+  const DCFHouseWalletBalance = await getBalanceWithRetry(DCF_HOUSE_WALLET) /1000000000;
+  const DCDFeeWalletBalance = await getBalanceWithRetry(DCD_FEE_WALLET) /1000000000;
+  const DCDHouseWalletBalance = await getBalanceWithRetry(DCD_HOUSE_WALLET) /1000000000;
+  // rest of your code...
+
+  console.log("Balances:");
+
+  console.log("DCFFeeWalletBalance:", DCFFeeWalletBalance);
+  console.log("DCFHouseWalletBalance:", DCFHouseWalletBalance);
+  console.log("DCDFeeWalletBalance:", DCDFeeWalletBalance);
+  console.log("DCDHouseWalletBalance:", DCDHouseWalletBalance);
+
+  return {
+    DCFFeeWalletBalance,
+    DCFHouseWalletBalance,
+    DCDFeeWalletBalance,
+    DCDHouseWalletBalance,
+  };
+};
+
+const getBlockTime = async () => {
+  const slot = await connection.getSlot();
+  const blockTime = await connection.getBlockTime(slot);
+  return blockTime;
+};
+
+const saveToDatabase = async () => {
+    try {
+      const balances = await getBalances();
+      const blockTime = await getBlockTime();
+  
+      const stmt = db.prepare(
+        `INSERT INTO balances (
+          blockTime,
+          DCFFeeWalletBalance,
+          DCFHouseWalletBalance,
+          DCDFeeWalletBalance,
+          DCDHouseWalletBalance
+        ) VALUES (?, ?, ?, ?, ?)`
+      );
+  
+      stmt.run(
+        blockTime,
+        balances.DCFFeeWalletBalance,
+        balances.DCFHouseWalletBalance,
+        balances.DCDFeeWalletBalance,
+        balances.DCDHouseWalletBalance,
+        (err) => {
+          if (err) {
+            console.error("Error inserting data:", err);
+            return;
+          }
+          console.log("Data inserted successfully.");
+        }
+      );
+    } catch (error) {
+      console.error("Failed to save to database:", error);
+    }
+  };
+  
+
+console.log();
+
+// Call saveToDatabase immediately and then every 5 minutes
+saveToDatabase();
+
+setInterval(saveToDatabase, 5 * 60 * 1000);
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const app = express();
+const dbPath = path.join(__dirname, "balances.db");
+console.log("Using database file at:", dbPath);
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("Error opening the database:", err.message);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get("/balances/range", (req, res) => {
+    const start = req.query.start;
+    const end = req.query.end;
+  
+    // Validate input
+    if (!start || !end) {
+      res.status(400).json({ error: "Please provide 'start' and 'end' query parameters in UNIX timestamp format." });
+      return;
+    }
+  
+    db.all("SELECT * FROM balances WHERE blockTime BETWEEN ? AND ?", [start, end], (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        balances: rows,
+      });
+    });
+  });
+  
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
