35b7768637b245a44511958e07c90592f60faed5
doctorsolana <127446248+doctorsolana@users.noreply.github.com>
2023-06-05 23:55:15 +0200

    Update Chart.tsx

--- client/src/Chart.tsx
+++ client/src/Chart.tsx
@@ -9,20 +9,34 @@ type ChartProps = {
   dataKey: string;
 };
 
-const Chart: React.FC<ChartProps> = ({ title, data, dataKey }) => (
-  <Box p={4} borderWidth={1} borderRadius="lg" background={"#16182e"}>
-    <Heading as="h3" size="lg" mb={4}>{title}</Heading>
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="blockTime" tickFormatter={(unixTime) => moment(unixTime).format('DD/MM/YYYY')} />
-        <YAxis domain={['auto', 'auto']} />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey={dataKey} stroke="#ffaf01" strokeWidth={2} activeDot={{ r: 8 }} />
-      </LineChart>
-    </ResponsiveContainer>
-  </Box>
-);
+const Chart: React.FC<ChartProps> = ({ title, data, dataKey }) => {
+  const CustomTooltip = ({ active, payload }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`${payload[0].value}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <Box p={4} borderWidth={1} borderRadius="lg" background={"#16182e"}>
+      <Heading as="h3" size="lg" mb={4}>{title}</Heading>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="blockTime" tickFormatter={(unixTime) => moment(unixTime).format('DD/MM/YYYY')} />
+          <YAxis domain={['auto', 'auto']} />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+          <Line type="monotone" dataKey={dataKey} stroke="#ffaf01" strokeWidth={4} dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </Box>
+  );
+};
 
 export default Chart;
