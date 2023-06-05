2acafa9fee74db4a7d4bca8e0e33c91b4d880348
doctorsolana <127446248+doctorsolana@users.noreply.github.com>
2023-06-05 18:58:05 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,24 +16,29 @@ function App() {
   const [data, setData] = useState<BalanceData[]>([]);
 
   useEffect(() => {
-    console.log("useEffect is being called");  // Added log
-    const start = Math.floor(Date.now() / 1000) - 30 * 24 * 60 * 60; 
-    const end = Math.floor(Date.now() / 1000); 
-    const url = `https://209.38.229.113.nip.io:3003/balances/range?start=${start}&end=${end}`;
-    console.log("URL to fetch", url);  // Added log
-    axios.get(url)
-      .then(response => {
-        console.log("Successful response", response);  // Added log
-        const balancesData = response.data.balances.map((item: any) => ({
-          ...item,
-          blockTime: new Date(item.blockTime * 1000),
-        }));
-        setData(balancesData);
-      })
-      .catch(error => {
-        console.error("Error in fetching data", error);  // Added log
-      });
-  }, []);
+  axios.get('https://209.38.229.113.nip.io:3003/latest-date')
+    .then(response => {
+      const latestBlockTime = response.data.latestBlockTime;
+      const start = latestBlockTime - 30 * 24 * 60 * 60; 
+      const url = `https://209.38.229.113.nip.io:3003/balances/range?start=${start}&end=${latestBlockTime}`;
+      
+      axios.get(url)
+        .then(response => {
+          const balancesData = response.data.balances.map((item: any) => ({
+            ...item,
+            blockTime: new Date(item.blockTime * 1000),
+          }));
+          setData(balancesData);
+        })
+        .catch(error => {
+          console.error("Error in fetching data", error);
+        });
+    })
+    .catch(error => {
+      console.error("Error in fetching latest date", error);
+    });
+}, []);
+
   
   const totalBalance = data.length > 0 ? data[0].DCFFeeWalletBalance + data[0].DCDFeeWalletBalance : 0;
 
